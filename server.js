135ef5c9ffea9af55f93f6b1ee39a38118cb469a
Arseniy Pelipas <pelipas@e-garant.ru>
2021-01-19 16:56:36 +0300

    Setup DB-object for json-server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const db = { users: [] };
+// Create 1000 users
+for (let i = 0; i < 1000; i++) {
+  db.users.push({ id: i, name: `user${i}` });
+}
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("index.js");
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
